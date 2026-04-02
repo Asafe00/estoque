2a55ddf21cc4dc7d8a08b1e9bfc6e2df40d0202e
Asafe00 <161603083+Asafe00@users.noreply.github.com>
2026-04-02 15:32:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function(){
   carregarProdutos();
   mostrarProdutos();
   carregarHistorico();
+  carregarMinimos();
 });
 if(formProduto){
   formProduto.addEventListener("submit", async function(event){
