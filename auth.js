5633890aa00c35c515a6efc30bc83da052d045e4
Asafe00 <161603083+Asafe00@users.noreply.github.com>
2026-04-08 17:40:01 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,9 +1,20 @@
 import { app } from "/estoque/firebase.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
 const auth = getAuth(app);
 
+const paginaAtual = window.location.pathname;
+
+if (paginaAtual.includes("login.html")) {
+  // 👉 se estiver no login, não faz nada
+} else {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = "/estoque/paginas/login.html";
+    }
+  });
+}
 
 // 🔹 REGISTRO
 const formRegistro = document.getElementById("formRegistro");
--- authGuard.js
+++ authGuard.js
@@ -4,8 +4,14 @@ from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
 const auth = getAuth(app);
 
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "/estoque/paginas/login.html";
-  }
-});
+const paginaAtual = window.location.pathname;
+
+if (paginaAtual.includes("login.html")) {
+  // 👉 se estiver no login, não faz nada
+} else {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = "/estoque/paginas/login.html";
+    }
+  });
+}
