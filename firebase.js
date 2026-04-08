fbcceefd7b8cc18e2f63d36a684b3dd0fefa4bad
Asafe00 <161603083+Asafe00@users.noreply.github.com>
2026-04-08 17:54:25 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/fireba
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD...",
+  apiKey: "AIzaSyDSt1sx09eqlcOpvjw8JpmzDaDvN5JBqTQ",
   authDomain: "estoque-empresa-20acc.firebaseapp.com",
   databaseURL: "https://estoque-empresa-20acc-default-rtdb.firebaseio.com",
   projectId: "estoque-empresa-20acc",
