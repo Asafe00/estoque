c55d013a141270d887c98ddb8908d43007d262e6
Asafe00 <161603083+Asafe00@users.noreply.github.com>
2026-04-09 14:57:47 -0300

    Add files via upload

--- authGuard.js
+++ authGuard.js
@@ -4,12 +4,13 @@ from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
 const auth = getAuth(app);
 
-const paginaAtual = window.location.pathname;
+const paginasPublicas = ["login.html", "registrar.html"];
 
-if (paginaAtual.includes("login.html") ||
-    paginaAtual.includes("registrar.html") {
-  // 👉 se estiver no login, não faz nada
-} else {
+const ehPublica = paginasPublicas.some(pagina =>
+  paginaAtual.includes(pagina)
+);
+
+if (!ehPublica) {
   onAuthStateChanged(auth, (user) => {
     if (!user) {
       window.location.href = "/estoque/paginas/login.html";
