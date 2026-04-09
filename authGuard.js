92af24940fd9a7bd636c6a2afe527979f7fff2d5
Asafe00 <161603083+Asafe00@users.noreply.github.com>
2026-04-09 14:52:42 -0300

    Add files via upload

--- authGuard.js
+++ authGuard.js
@@ -6,7 +6,8 @@ const auth = getAuth(app);
 
 const paginaAtual = window.location.pathname;
 
-if (paginaAtual.includes("login.html"||"registrar.html")) {
+if (paginaAtual.includes("login.html") ||
+    paginaAtual.includes("registrar.html") {
   // 👉 se estiver no login, não faz nada
 } else {
   onAuthStateChanged(auth, (user) => {
