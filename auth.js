598160f6966f0f6bae34d75522c1006429c26085
Asafe00 <161603083+Asafe00@users.noreply.github.com>
2026-04-10 11:01:17 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,31 +1,30 @@
 import { app } from "/estoque/firebase.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
 const auth = getAuth(app);
 
+// 🔹 SE JÁ ESTIVER LOGADO, REDIRECIONA
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = "/estoque/index.html";
+  }
+});
 
 // 🔹 REGISTRO
 const formRegistro = document.getElementById("formRegistro");
-
-if (formRegistro){
-  formRegistro.addEventListener("submit", async function(event){
+if (formRegistro) {
+  formRegistro.addEventListener("submit", async function(event) {
     event.preventDefault();
-
     const nome = document.getElementById("nome").value;
     const senha = document.getElementById("senha").value;
-
-    // 🔥 Firebase precisa de email → adaptamos
     const email = nome + "@sistema.com";
-
     try {
       await createUserWithEmailAndPassword(auth, email, senha);
-
       alert("Usuário registrado!");
       window.location.href = "/estoque/index.html";
-
-    } catch (erro){
-      if(erro.code === "auth/email-already-in-use"){
+    } catch (erro) {
+      if (erro.code === "auth/email-already-in-use") {
         alert("Esse usuário já existe!");
       } else {
         alert("Erro ao registrar");
@@ -35,26 +34,19 @@ if (formRegistro){
   });
 }
 
-
 // 🔹 LOGIN
 const formLogin = document.getElementById("formLogin");
-
-if(formLogin){
-  formLogin.addEventListener("submit", async function(event){
+if (formLogin) {
+  formLogin.addEventListener("submit", async function(event) {
     event.preventDefault();
-
     const nomeDigitado = document.getElementById("loginNome").value;
     const senhaDigitada = document.getElementById("loginSenha").value;
-
     const email = nomeDigitado + "@sistema.com";
-
     try {
       await signInWithEmailAndPassword(auth, email, senhaDigitada);
-
       alert("Login realizado com sucesso!");
       window.location.href = "/estoque/index.html";
-
-    } catch (erro){
+    } catch (erro) {
       alert("Usuário ou senha inválidos!");
       console.error(erro);
     }
